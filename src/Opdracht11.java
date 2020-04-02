830a824039d553e5a4376fc9a608703c1865ee97
Matrixgek91 <jschouw1@gmail.com>
2020-04-02 14:17:31 +0200

    Updated with assignment 11

---
+++ src/Opdracht11.java
@@ -0,0 +1,29 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Opdracht11 {
+    public static void main(String[] args) {
+
+        int[] productPrice = new int[]{295, 499, 549, 780, 885};
+        double totalPrice;
+        int productNumber;
+        int unitsSold;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the product number and how many units have been sold.\n");
+
+        System.out.print("Please enter the product number: ");
+        productNumber = scanner.nextInt();
+        System.out.print("Please enter the number of units sold: ");
+        unitsSold = scanner.nextInt();
+
+        totalPrice = (productPrice[(productNumber - 1)] * unitsSold);
+
+        System.out.println("The total retail value is: " +(totalPrice / 100));
+
+
+
+
+    }
+}
