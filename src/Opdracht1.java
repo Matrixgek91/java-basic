c877e31014a52fb539a04c053e83a49f77830fe5
Matrixgek91 <jschouw1@gmail.com>
2020-04-02 12:02:39 +0200

    Updated with assignment 1

--- src/Opdracht1.java
+++ src/Opdracht1.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Opdracht1 {
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the following to construct a sentence");
+
+        System.out.print("article: ");
+                String article = scanner.nextLine();
+        System.out.print("noun: ");
+                String noun = scanner.nextLine();
+        System.out.print("verb: ");
+            String verb = scanner.nextLine();
+
+        System.out.println("Your sentence is: " + article + " " +  noun + " " + verb);
     }
 }
