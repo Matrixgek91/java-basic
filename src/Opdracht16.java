9f1337a78cc73400826d4eb33618930a441c7304
Matrixgek91 <jschouw1@gmail.com>
2020-04-02 15:01:19 +0200

    Updated with assignment 15

---
+++ src/Opdracht16.java
@@ -0,0 +1,13 @@
+public class Opdracht16 {
+    public static void main(String[] args) {
+
+        int myNum = 5;
+
+        for(int i = 0; i < 10; i++){
+            System.out.print(myNum + ", ");
+            myNum = myNum + 4;
+        }
+
+
+    }
+}
