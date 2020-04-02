77e7195a31e339fc582cc16bac6bb80aadc5a0f2
Matrixgek91 <jschouw1@gmail.com>
2020-04-02 13:47:04 +0200

    Updated with assignment 8

---
+++ src/Opdracht8.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Opdracht8 {
+    public static void main(String[] args) {
+
+        int[] myNum = new int[3];
+        int largestNumber = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter three numbers.\n");
+
+        System.out.print("Number one: ");
+        myNum[0] = scanner.nextInt();
+        System.out.print("Number two: ");
+        myNum[1] = scanner.nextInt();
+        System.out.print("Number three: ");
+        myNum[2] = scanner.nextInt();
+
+        for(int i : myNum){
+            if(i > largestNumber){
+                largestNumber = i;
+            }
+        }
+
+        System.out.println("\nThe largest of these numbers is: " + largestNumber);
+
+
+    }
+}
