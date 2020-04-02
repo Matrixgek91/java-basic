ace383dc2a2c6c2da003ad2dcf6eaccf6a454292
Matrixgek91 <jschouw1@gmail.com>
2020-04-02 14:53:33 +0200

    Updated with assignment 15

--- src/Opdracht15.java
+++ src/Opdracht15.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Opdracht14 {
+public class Opdracht15 {
     public static void main(String[] args) {
 
         ArrayList<Integer> numList = new ArrayList<Integer>();
@@ -11,11 +11,11 @@ public class Opdracht14 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter numbers between 1 and 20. When done, type exit. \nPlease enter numbers:");
-            while(scanner.hasNextInt()){
+        System.out.println("Enter odd numbers between 1 and 20. When done, type exit. \nPlease enter numbers:");
+        while(scanner.hasNextInt()){
             myNum = scanner.nextInt();
-            if(myNum > 0 && myNum < 21){
-               numList.add(myNum);
+            if(myNum > 0 && myNum < 21 && (myNum % 2) == 1){
+                numList.add(myNum);
             }else{
                 System.out.println("Invalid number, please try again.");
             }
