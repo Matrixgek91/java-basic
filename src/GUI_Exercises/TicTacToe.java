35252bb4bc2fdee83404717422947e3e9a6c966b
Matrixgek91 <jschouw1@gmail.com>
2020-04-06 10:42:33 +0200

    Started on TicTacToe, work in progress

--- src/GUI_Exercises/TicTacToe.java
+++ src/GUI_Exercises/TicTacToe.java
@@ -9,23 +9,37 @@ public class TicTacToe extends JFrame implements ActionListener {
 
     JLabel display = new JLabel("Hoi");
 
+    String TL;
+    String TC;
+    String TR;
+    String ML;
+    String MC;
+    String MR;
+    String BL;
+    String BC;
+    String BR;
+
     String[] labels = {
+            TL, TC, TR,
+            ML, MC, MR,
+            BL, BC, BR
+    };
+    String[] names = {
             "TL", "TC", "TR",
-            "CL", "CC", "CR",
+            "MR", "MC", "MR",
             "BL", "BC", "BR"
     };
 
-
-
-
     public TicTacToe(){
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4,4));
+        panel.setLayout(new GridLayout(3,3));
+        panel.setPreferredSize(new Dimension(400,400));
         getContentPane().add(display, BorderLayout.NORTH);
         getContentPane().add(panel, BorderLayout.SOUTH);
 
         for (int i = 0 ; i < 9 ; i++){
             JButton button = new JButton(labels [i]);
+            button.setName(names [i]);
             panel.add(button);
             button.addActionListener(this);
         }
@@ -35,7 +49,7 @@ public class TicTacToe extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
-        ticTacToe.setSize(400,400);
+        ticTacToe.setSize(400,500);
         ticTacToe.setVisible(true);
         ticTacToe.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -44,6 +58,8 @@ public class TicTacToe extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        JButton o = (JButton)e.getSource();
+        String currentButton = o.getName();
+        System.out.println(currentButton);
     }
 }
